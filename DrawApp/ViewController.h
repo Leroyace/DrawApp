bc4a6b4d23ad5f9164d7d93a20973a9a1c72a424
Leroy Yu Tse <LeroyTse@leroys-macbook-air.local>
2016-03-23 21:33:26 +1300

    Initial Commit

---
+++ DrawApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawApp
+//
+//  Created by Leroy Yu Tse on 23/03/16.
+//  Copyright © 2016 Leroy Yu Tse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DrawApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DrawApp
+//
+//  Created by Leroy Yu Tse on 23/03/16.
+//  Copyright © 2016 Leroy Yu Tse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
